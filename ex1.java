b293992f6f9695a20681d595a605b34ed5be85d4
Ben Fedoruk <290071359+bfedoruk-sls@users.noreply.github.com>
2026-07-21 14:16:11 -0400

    examples 1 and 2

---
+++ ex1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your first name: ");
+
+        String name = input.next();
+
+        input.close();
+    }
+}
